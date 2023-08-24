50ef24d31dde8a6ed2bc8372236a0f6f8924d3d7
NosbaoJ92 <joabson.nogueira@hotmail.com>
2023-08-24 15:59:20 -0300

    segundo commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
-function abrirArtefato() {
-          
+function AbrirWEB() {
+    document.getElementById("projetos-web").style.display = "block";    
+    document.getElementById("projetos-mobile").style.display = "none";      
+    }
+function AbrirMOBILE() {
+    document.getElementById("projetos-web").style.display = "none";    
+    document.getElementById("projetos-mobile").style.display = "block";      
     }
